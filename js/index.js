1e0f8f5391e2ce34005d71937c5cc65a43af5fa6
Fernando Echevarria Gianello <fernandoechevarriagianello@iMac-de-Fernando.local>
2022-05-12 17:01:39 +0200

    https://github.com/ferechegia/lab-canvas-race-car.git

--- js/index.js
+++ js/index.js
@@ -1,7 +1,69 @@
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+let gameOver = false;
+let carX = 250;
+let intervalId = 0;
+canvas.style.border = '2px solid black'
+let isGoingLeft = false;
+let isGoingRight = false;
+let carSpeedValue = 3;
+
+let roadImg = new Image ()
+roadImg.src = '../images/road.png'
+
+let startScreen = document.querySelector('.game-intro')
+
+let carImage = new Image()
+carImage.src = '../images/car.png'
+
+let displayScreen = document.querySelector('.game-intro');
+
+
 window.onload = () => {
+  canvas.style.display = 'none'
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    displayScreen.style.display = 'none'
+    canvas.style.display = 'block'
+    ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height)
+    ctx.drawImage(carImage, carX, 450, 100, 200)
+
+    if (isGoingLeft) {
+      if (carX > 0) {
+        carX -= carSpeedValue;
+      }
+    } else if (isGoingRight) {
+      if (carX < canvas.width - 250) {
+        carX += carSpeedValue;
+      }
+    }
+    if(gameOver === false) {
+      requestAnimationFrame(startGame);
+    } 
+  }
 };
+
+
+
+function draw() {
+
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
+}
+
+  document.addEventListener("keydown", event => {
+    console.log(event)
+  if (event.code === "ArrowLeft") {
+    isGoingLeft = true;
+  }
+  if (event.code === "ArrowRight") {
+    isGoingRight = true;
+  }
+});
+
+document.addEventListener("keyup", event => {
+  isGoingLeft = false;
+  isGoingRight = false;
+});
